4d9b032f091d72619fb2b5857f12bf0c29583aa3
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2024-10-04 10:44:16 +0200

    fix categories

--- src/main/kotlin/gay/j10a1n15/customscoreboard/config/MainConfig.kt
+++ src/main/kotlin/gay/j10a1n15/customscoreboard/config/MainConfig.kt
@@ -1,25 +1,25 @@
 package gay.j10a1n15.customscoreboard.config;
 
-import com.teamresourceful.resourcefulconfig.api.annotations.*;
-import com.teamresourceful.resourcefulconfig.api.types.options.EntryType;
-import gay.j10a1n15.customscoreboard.config.category.TitleAndFooterConfig;
-import gay.j10a1n15.customscoreboard.feature.customscoreboard.ScoreboardEntry;
+import com.teamresourceful.resourcefulconfig.api.annotations.*
+import com.teamresourceful.resourcefulconfig.api.types.options.EntryType
+import gay.j10a1n15.customscoreboard.config.category.TitleAndFooterConfig
+import gay.j10a1n15.customscoreboard.feature.customscoreboard.ScoreboardEntry
 
-@ConfigInfo.Provider(InfoProvider.class)
+@ConfigInfo.Provider(InfoProvider::class)
 @Config(
-        value = "customscoreboard/config",
-        categories = {
-                TitleAndFooterConfig.class
-        }
+    value = "customscoreboard/config",
+    categories = [
+        TitleAndFooterConfig::class,
+    ],
 )
-public class MainConfig {
+object MainConfig {
 
     @ConfigEntry(
-            id = "Appearance",
-            type = EntryType.ENUM
+        id = "Appearance",
+        type = EntryType.ENUM,
     )
     @ConfigOption.Draggable
     @Comment("Edit this list to change the order of appearance of the scoreboard.")
-    public static ScoreboardEntry[] appearance = ScoreboardEntry.getEntries().toArray(new ScoreboardEntry[0]);
+    var appearance: Array<ScoreboardEntry> = ScoreboardEntry.entries.toTypedArray()
 
 }
--- src/main/kotlin/gay/j10a1n15/customscoreboard/config/category/TitleAndFooterConfig.kt
+++ src/main/kotlin/gay/j10a1n15/customscoreboard/config/category/TitleAndFooterConfig.kt
@@ -1,16 +1,16 @@
 package gay.j10a1n15.customscoreboard.config.category;
 
-import com.teamresourceful.resourcefulconfig.api.annotations.Category;
-import com.teamresourceful.resourcefulconfig.api.annotations.ConfigEntry;
-import com.teamresourceful.resourcefulconfig.api.types.options.EntryType;
-import gay.j10a1n15.customscoreboard.utils.rendering.Alignment;
+import com.teamresourceful.resourcefulconfig.api.annotations.Category
+import com.teamresourceful.resourcefulconfig.api.annotations.ConfigEntry
+import com.teamresourceful.resourcefulconfig.api.types.options.EntryType
+import gay.j10a1n15.customscoreboard.utils.rendering.Alignment
 
 @Category("Title and Footer")
-public class TitleAndFooterConfig {
+object TitleAndFooterConfig {
 
     @ConfigEntry(
-            id = "Title Alignment",
-            type = EntryType.ENUM
+        id = "Title Alignment",
+        type = EntryType.ENUM
     )
-    public static Alignment titleAlignment = Alignment.CENTER;
+    var titleAlignment: Alignment = Alignment.CENTER
 }
