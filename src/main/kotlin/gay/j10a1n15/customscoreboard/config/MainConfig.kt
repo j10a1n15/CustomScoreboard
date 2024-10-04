d6304e318723a6b3c470433129e01ebd4fca4d50
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2024-10-04 11:49:04 +0200

    load title and footer config from data class
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/gay/j10a1n15/customscoreboard/config/MainConfig.kt
+++ src/main/kotlin/gay/j10a1n15/customscoreboard/config/MainConfig.kt
@@ -2,15 +2,13 @@ package gay.j10a1n15.customscoreboard.config;
 
 import com.teamresourceful.resourcefulconfig.api.annotations.*
 import com.teamresourceful.resourcefulconfig.api.types.options.EntryType
-import gay.j10a1n15.customscoreboard.config.category.TitleAndFooterConfig
+import gay.j10a1n15.customscoreboard.config.objects.TitleOrFooterObject
 import gay.j10a1n15.customscoreboard.feature.customscoreboard.ScoreboardEntry
 
 @ConfigInfo.Provider(InfoProvider::class)
 @Config(
     value = "customscoreboard/config",
-    categories = [
-        TitleAndFooterConfig::class,
-    ],
+    categories = [],
 )
 object MainConfig {
 
@@ -22,4 +20,17 @@ object MainConfig {
     @Comment("Edit this list to change the order of appearance of the scoreboard.")
     var appearance: Array<ScoreboardEntry> = ScoreboardEntry.entries.toTypedArray()
 
+    @ConfigEntry(
+        id = "Title Options",
+        type = EntryType.OBJECT,
+    )
+    @Comment("Edit the title of the scoreboard.")
+    val title: TitleOrFooterObject = TitleOrFooterObject()
+
+    @ConfigEntry(
+        id = "Footer Options",
+        type = EntryType.OBJECT,
+    )
+    @Comment("Edit the footer of the scoreboard.")
+    val footer: TitleOrFooterObject = TitleOrFooterObject()
 }
