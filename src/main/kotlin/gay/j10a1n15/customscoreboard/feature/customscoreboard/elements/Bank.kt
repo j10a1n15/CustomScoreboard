5f427e0a0684dbc999d05102081ebb915f11d92a
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2024-10-08 10:50:51 +0200

    update coop bank
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/gay/j10a1n15/customscoreboard/feature/customscoreboard/elements/Bank.kt
+++ src/main/kotlin/gay/j10a1n15/customscoreboard/feature/customscoreboard/elements/Bank.kt
@@ -3,11 +3,14 @@ package gay.j10a1n15.customscoreboard.feature.customscoreboard.elements
 import gay.j10a1n15.customscoreboard.utils.NumberUtils.format
 import tech.thatgravyboat.skyblockapi.api.location.SkyblockIsland
 import tech.thatgravyboat.skyblockapi.api.profile.CurrencyAPI
+import tech.thatgravyboat.skyblockapi.api.profile.profile.ProfileAPI
 
 object Bank : ScoreboardElement() {
-    override fun getDisplay() =
-        // todo: use isCoop profile
-        "Bank: §6${CurrencyAPI.personalBank.format()}" + if (CurrencyAPI.coopBank > 0) " §7/§6${CurrencyAPI.coopBank.format()}" else ""
+    override fun getDisplay() = if (ProfileAPI.coop) {
+        "Bank: §6${CurrencyAPI.personalBank.format()}§7/§6${CurrencyAPI.coopBank.format()}"
+    } else {
+        "Bank: §6${CurrencyAPI.coopBank.format()}"
+    }
 
     override fun showIsland() = !SkyblockIsland.inAnyIsland(SkyblockIsland.THE_RIFT)
 
