ca064202a1b22f34e9c33494e669e70e23b2186a
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2024-10-09 22:41:30 +0200

    improve fps
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/gay/j10a1n15/customscoreboard/utils/TextUtils.kt
+++ src/main/kotlin/gay/j10a1n15/customscoreboard/utils/TextUtils.kt
@@ -1,12 +1,26 @@
 package gay.j10a1n15.customscoreboard.utils
 
 import net.minecraft.network.chat.Component
-import tech.thatgravyboat.skyblockapi.utils.extentions.trimIgnoreColor
 import tech.thatgravyboat.skyblockapi.utils.text.Text
+
+private val colorCodesStart = Regex("^(?<start>(§.| )*)(?!§.| )")
+private val colorCodesEnd = Regex("^(?<end>(.§| )*)(?!.§| )")
 
 object TextUtils {
 
     fun String.toComponent() = Component.literal(this)
+
+    fun String.trimStartIgnoreColor(): String {
+        val start = colorCodesStart.find(this)?.groups?.get("start")?.value ?: ""
+        val trimmed = this.removePrefix(start)
+        return start.replace(" ", "") + trimmed
+    }
+
+    fun String.trimEndIgnoreColor(): String {
+        val end = colorCodesEnd.find(this.reversed())?.groups?.get("end")?.value?.reversed() ?: ""
+        val trimmed = this.removeSuffix(end)
+        return trimmed + end.replace(" ", "")
+    }
 
     fun Component.trim(): Component {
         var siblings = this.toFlatList().toMutableList()
@@ -14,8 +28,8 @@ object TextUtils {
         val last = siblings.lastOrNull()
         siblings = siblings.drop(1).dropLast(1).toMutableList()
 
-        siblings.addFirst(first?.let { Component.literal((it.string + "|").trimIgnoreColor().removeSuffix("|")).withStyle(it.style) })
-        siblings.addLast(last?.let { Component.literal(("|" + it.string).trimIgnoreColor().removePrefix("|")).withStyle(it.style) })
+        siblings.addFirst(first?.let { Component.literal(it.string.trimStartIgnoreColor()).withStyle(it.style) })
+        siblings.addLast(last?.let { Component.literal(it.string.trimEndIgnoreColor()).withStyle(it.style) })
 
         return Text.join(*siblings.toTypedArray())
     }
