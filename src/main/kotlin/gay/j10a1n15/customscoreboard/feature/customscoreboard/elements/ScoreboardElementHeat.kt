c0bd58e021709371d2d1766ccd20d8b7a47f1518
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2024-10-09 23:49:09 +0200

    added coordinate check to heat
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/gay/j10a1n15/customscoreboard/feature/customscoreboard/elements/ScoreboardElementHeat.kt
+++ src/main/kotlin/gay/j10a1n15/customscoreboard/feature/customscoreboard/elements/ScoreboardElementHeat.kt
@@ -2,6 +2,7 @@ package gay.j10a1n15.customscoreboard.feature.customscoreboard.elements
 
 import tech.thatgravyboat.skyblockapi.api.area.mining.HollowsAPI
 import tech.thatgravyboat.skyblockapi.api.location.SkyblockIsland
+import tech.thatgravyboat.skyblockapi.helpers.McPlayer
 
 object ScoreboardElementHeat : ScoreboardElement() {
     override fun getDisplay() = buildString {
@@ -10,7 +11,7 @@ object ScoreboardElementHeat : ScoreboardElement() {
         else append("§c${HollowsAPI.heat}♨")
     }
 
-    override fun showWhen() = HollowsAPI.heat != 0
+    override fun showWhen() = HollowsAPI.heat != 0 || McPlayer.self?.y?.let { it <= 64 } == true
 
     override fun showIsland() = SkyblockIsland.inAnyIsland(SkyblockIsland.CRYSTAL_HOLLOWS)
 
