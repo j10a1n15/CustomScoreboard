e2b8de7eaecf351aac75ad23ed673ee7169d168e
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2024-10-12 22:03:18 +0200

    fix jacobs contest
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/gay/j10a1n15/customscoreboard/feature/customscoreboard/events/EventJacobsContest.kt
+++ src/main/kotlin/gay/j10a1n15/customscoreboard/feature/customscoreboard/events/EventJacobsContest.kt
@@ -1,17 +1,31 @@
 package gay.j10a1n15.customscoreboard.feature.customscoreboard.events
 
 import gay.j10a1n15.customscoreboard.utils.Utils.nextAfter
-import tech.thatgravyboat.skyblockapi.helpers.McClient
+import tech.thatgravyboat.skyblockapi.api.events.base.Subscription
+import tech.thatgravyboat.skyblockapi.api.events.info.ScoreboardUpdateEvent
+import tech.thatgravyboat.skyblockapi.utils.regex.RegexUtils.anyMatch
 
 object EventJacobsContest : Event() {
     // TODO: use scoreboard update event
-    override fun getDisplay() = buildList {
-        val lines = McClient.scoreboard
-        add("§eJacob's Contest")
-        lines.nextAfter("Jacob's Contest")?.let { add(it) }
-        lines.nextAfter("Jacob's Contest", 2)?.let { add(it) }
-        lines.nextAfter("Jacob's Contest", 3)?.let { add(it) }
-    }
+    override fun getDisplay() = formattedLines
 
     override val configLine = "Jacob's Contest"
+
+
+    private val contestRegex = "Jacob's Contest".toRegex()
+
+    private val formattedLines = mutableListOf<String>()
+
+    @Subscription
+    fun onScoreboardUpdate(event: ScoreboardUpdateEvent) {
+        formattedLines.clear()
+
+        contestRegex.anyMatch(event.new) {
+            formattedLines.add(it.string)
+            event.new.nextAfter(it.string)?.let { formattedLines.add(it) }
+            event.new.nextAfter(it.string, 2)?.let { formattedLines.add(it) }
+            event.new.nextAfter(it.string, 3)?.let { formattedLines.add(it) }
+        }
+
+    }
 }
