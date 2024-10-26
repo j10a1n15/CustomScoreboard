f1a27666f27e93f4a5e1c9a14f9e903f60156090
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2024-10-26 13:26:52 +0200

    add roomId
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/gay/j10a1n15/customscoreboard/feature/customscoreboard/elements/ElementLobby.kt
+++ src/main/kotlin/gay/j10a1n15/customscoreboard/feature/customscoreboard/elements/ElementLobby.kt
@@ -2,18 +2,32 @@ package gay.j10a1n15.customscoreboard.feature.customscoreboard.elements
 
 import tech.thatgravyboat.skyblockapi.api.events.base.Subscription
 import tech.thatgravyboat.skyblockapi.api.events.hypixel.ServerChangeEvent
+import tech.thatgravyboat.skyblockapi.api.events.info.ScoreboardUpdateEvent
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
 object ElementLobby : Element() {
     private val formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy")
     private var lobbyCode: String? = null
+    private var roomId: String? = null
 
-    override fun getDisplay() = "§7" + LocalDate.now().format(formatter) + " §8$lobbyCode"
+    override fun getDisplay() = "§7" + LocalDate.now().format(formatter) + " §8$lobbyCode" + if (roomId != null) " §8$roomId" else ""
 
     override fun showWhen() = lobbyCode != null
 
     override val configLine = "Lobby"
+
+
+    private val roomIdRegex = "\\d+\\/\\d+\\/\\d+ \\w+ (?<roomId>[\\w,-]+)".toRegex()
+
+    @Subscription
+    fun onScoreboardUpdate(event: ScoreboardUpdateEvent) {
+        for (line in event.new) {
+            roomId = roomIdRegex.find(line)?.groups["roomId"]?.value ?: continue
+            return
+        }
+        roomId = null
+    }
 
     @Subscription
     fun onServerChange(event: ServerChangeEvent) {
