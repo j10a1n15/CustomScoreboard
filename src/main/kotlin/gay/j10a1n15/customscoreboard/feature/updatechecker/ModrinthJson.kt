4feb4b37a6580532bb471c4044b689e60c6f8643
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2024-11-11 22:25:21 +0100

    update checker cleanup
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/gay/j10a1n15/customscoreboard/feature/updatechecker/ModrinthJson.kt
+++ src/main/kotlin/gay/j10a1n15/customscoreboard/feature/updatechecker/ModrinthJson.kt
@@ -1,9 +1,9 @@
 package gay.j10a1n15.customscoreboard.feature.updatechecker
 
 import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
 
 data class ModrinthJson(
     @Expose val name: String,
-    @Expose val version_number: String,
-    @Expose val date_published: String,
+    @Expose @SerializedName("version_number") val versionNumber: String,
 )
