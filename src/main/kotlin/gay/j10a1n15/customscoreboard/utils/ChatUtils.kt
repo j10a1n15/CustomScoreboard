9aea92fc8cabd23fc81f32f9b72fd9dd7e57bf17
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2024-11-16 22:05:56 +0100

    fix chatutils
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/gay/j10a1n15/customscoreboard/utils/ChatUtils.kt
+++ src/main/kotlin/gay/j10a1n15/customscoreboard/utils/ChatUtils.kt
@@ -1,9 +1,9 @@
 package gay.j10a1n15.customscoreboard.utils
 
-import com.teamresourceful.resourcefullibkt.common.url
 import gay.j10a1n15.customscoreboard.utils.TextUtils.toComponent
 import tech.thatgravyboat.skyblockapi.helpers.McPlayer
 import tech.thatgravyboat.skyblockapi.utils.text.Text
+import tech.thatgravyboat.skyblockapi.utils.text.TextStyle.url
 
 object ChatUtils {
 
