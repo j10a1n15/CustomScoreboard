11cc8e221603c9d2930a8281eec5b7ebce92e4cc
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2024-11-19 19:38:24 +0100

    finish number tracking
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/gay/j10a1n15/customscoreboard/feature/customscoreboard/elements/ElementPurse.kt
+++ src/main/kotlin/gay/j10a1n15/customscoreboard/feature/customscoreboard/elements/ElementPurse.kt
@@ -1,12 +1,21 @@
 package gay.j10a1n15.customscoreboard.feature.customscoreboard.elements
 
 import gay.j10a1n15.customscoreboard.feature.customscoreboard.CustomScoreboardRenderer
+import gay.j10a1n15.customscoreboard.feature.customscoreboard.NumberTrackingElement
 import gay.j10a1n15.customscoreboard.utils.NumberUtils.format
 import tech.thatgravyboat.skyblockapi.api.location.SkyBlockIsland
 import tech.thatgravyboat.skyblockapi.api.profile.CurrencyAPI
 
-object ElementPurse : Element() {
-    override fun getDisplay() = CustomScoreboardRenderer.formatNumberDisplayDisplay("Purse", CurrencyAPI.purse.format(), "§6")
+object ElementPurse : Element(), NumberTrackingElement {
+    override var previousAmount: Long = CurrencyAPI.purse.toLong()
+    override var temporaryChangeDisplay: String? = null
+    override val numberColor = "§6"
+
+    override fun getDisplay(): String {
+        checkDifference(CurrencyAPI.purse.toLong())
+        val line = CurrencyAPI.purse.format() + temporaryChangeDisplay.orEmpty()
+        return CustomScoreboardRenderer.formatNumberDisplayDisplay("Bank", line, numberColor)
+    }
 
     override fun showIsland() = !SkyBlockIsland.inAnyIsland(SkyBlockIsland.THE_RIFT)
 
