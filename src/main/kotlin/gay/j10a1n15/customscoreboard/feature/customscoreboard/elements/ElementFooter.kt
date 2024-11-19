5c705bd89187d90fe28a2248558397e216aee5f6
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2024-11-19 19:31:32 +0100

    add custom colors to custom text
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
    (cherry picked from commit 600cf145d722d4c0d10486a63611199c094cafe8)

--- src/main/kotlin/gay/j10a1n15/customscoreboard/feature/customscoreboard/elements/ElementFooter.kt
+++ src/main/kotlin/gay/j10a1n15/customscoreboard/feature/customscoreboard/elements/ElementFooter.kt
@@ -4,9 +4,11 @@ import gay.j10a1n15.customscoreboard.config.MainConfig
 import gay.j10a1n15.customscoreboard.feature.customscoreboard.ScoreboardLine.Companion.align
 
 object ElementFooter : Element() {
-    override fun getDisplay() = MainConfig.footer.let {
-        if (it.useCustomText) it.text align it.alignment else "§ewww.hypixel.net" align it.alignment
+    override fun getDisplay() = MainConfig.footer.let { f ->
+        if (f.useCustomText) f.text.formatFooter().map { it align f.alignment } else "§ewww.hypixel.net" align f.alignment
     }
 
     override val configLine: String = "§ewww.hypixel.net"
+
+    fun String.formatFooter() = this.replace("&&", "§").split("\\n")
 }
