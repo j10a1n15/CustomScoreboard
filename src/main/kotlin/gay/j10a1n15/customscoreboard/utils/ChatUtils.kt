969d5535c355e98c8193059e0bd5291e8ede69d6
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2024-11-22 16:02:08 +0100

    fix update notification appearing in actionbar
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
    
    (cherry picked from commit 64beece2c7f46628a98b29c540011b5044ed8a8b)
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/gay/j10a1n15/customscoreboard/utils/ChatUtils.kt
+++ src/main/kotlin/gay/j10a1n15/customscoreboard/utils/ChatUtils.kt
@@ -1,17 +1,16 @@
 package gay.j10a1n15.customscoreboard.utils
 
-import gay.j10a1n15.customscoreboard.utils.TextUtils.toComponent
-import tech.thatgravyboat.skyblockapi.helpers.McPlayer
 import tech.thatgravyboat.skyblockapi.utils.text.Text
+import tech.thatgravyboat.skyblockapi.utils.text.Text.send
 import tech.thatgravyboat.skyblockapi.utils.text.TextStyle.url
 
 object ChatUtils {
 
     fun chat(text: String) {
-        McPlayer.self?.sendSystemMessage(text.toComponent())
+        Text.of(text).send()
     }
 
     fun link(text: String, link: String) {
-        McPlayer.self?.sendSystemMessage(Text.of(text) { this.url = link })
+        Text.of(text) { this.url = link }.send()
     }
 }
